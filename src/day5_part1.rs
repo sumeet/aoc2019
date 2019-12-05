da4c230bc255fb3cf53e53e4101515ce44118f8f
Sumeet Agarwal <sumeet.a@gmail.com>
2019-12-05 01:28:50 -0500

    wip it up

--- src/day5_part1.rs
+++ src/day5_part1.rs
@@ -1,67 +1,63 @@
-use crate::day5::Instruction::{Add1, Multiply2, Input3, Output4, Halt99};
-use crate::day5::ParameterMode::{PositionMode, ImmediateMode};
+use crate::day5_part1::Instruction::{Add1, Multiply2, Input3, Output4, Halt99};
+use crate::day5_part1::ParameterMode::{PositionMode0, ImmediateMode1};
 
 #[derive(Debug)]
 enum Instruction {
     Add1(ParameterMode, ParameterMode, ParameterMode),
     Multiply2(ParameterMode, ParameterMode, ParameterMode),
     Input3,
-    Output4,
+    Output4(ParameterMode),
     Halt99,
 }
 
 impl Instruction {
     fn parse(s: &str) -> Self {
-        println!("parsing {} as instruction", s);
-        let parsed = if s.ends_with("01") {
+        let parsed = if s.ends_with("1") {
             let s = format!("{:0>5}", s);
             let third_param_mode = ParameterMode::parse(s.chars().nth(0).unwrap());
             let second_param_mode = ParameterMode::parse(s.chars().nth(1).unwrap());
             let first_param_mode = ParameterMode::parse(s.chars().nth(2).unwrap());
             Add1(first_param_mode, second_param_mode, third_param_mode)
-        } else if s.ends_with("02") {
+        } else if s.ends_with("2") {
             let s = format!("{:0>5}", s);
             let third_param_mode = ParameterMode::parse(s.chars().nth(0).unwrap());
             let second_param_mode = ParameterMode::parse(s.chars().nth(1).unwrap());
             let first_param_mode = ParameterMode::parse(s.chars().nth(2).unwrap());
             Multiply2(first_param_mode, second_param_mode, third_param_mode)
-        } else if s == "1" {
-            Add1(PositionMode, PositionMode, PositionMode)
-        } else if s == "2" {
-            Multiply2(PositionMode, PositionMode, PositionMode)
+        } else if s.ends_with("4") {
+            let s = format!("{:0>3}", s);
+            let first_param_mode = ParameterMode::parse(s.chars().nth(0).unwrap());
+            Output4(first_param_mode)
         } else if s == "3" {
             Input3
-        } else if s == "4" {
-            Output4
         } else if s == "99" {
             Halt99
         } else {
             panic!(format!("unable to parse instruction {}", s))
         };
-        println!("parsed {:?}", parsed);
         parsed
     }
 }
 
 #[derive(Debug)]
 enum ParameterMode {
-    PositionMode,
-    ImmediateMode,
+    PositionMode0,
+    ImmediateMode1,
 }
 
 impl ParameterMode {
     fn parse(c: char) -> Self {
         match c {
-            '0' => PositionMode,
-            '1' => ImmediateMode,
+            '0' => PositionMode0,
+            '1' => ImmediateMode1,
             _ => panic!(format!("unable to parse param mode {:?}", c))
         }
     }
 }
 
 
-#[aoc(day5, part1)]
-pub fn solve_part1(input: &str) -> String {
+#[aoc(day5, part2)]
+pub fn solve_part2(input: &str) -> String {
     let proggy : Vec<String> = input.lines().nth(0).unwrap().split(',').map(|s| s.to_owned()).collect();
     run_proggy(proggy)
 }
@@ -86,13 +82,13 @@ fn run_proggy(mut proggy: Vec<String>) -> String {
                 current_pos += 4;
             },
             Input3 => {
-                let position = get_first_param(&proggy, current_pos, ImmediateMode) as usize;
+                let position = get_first_param(&proggy, current_pos, ImmediateMode1) as usize;
                 // this is the only input instruction...
                 proggy[position] = "1".to_owned();
                 current_pos += 2;
             },
-            Output4 => {
-                let param = get_first_param(&proggy, current_pos, PositionMode);
+            Output4(mode) => {
+                let param = get_first_param(&proggy, current_pos, mode);
                 println!("{}", param);
                 current_pos += 2;
             },
@@ -106,10 +102,10 @@ fn run_proggy(mut proggy: Vec<String>) -> String {
 fn get_first_param(proggy: &[String], instruction_pos: usize, mode: ParameterMode) -> isize {
     let i = proggy[instruction_pos + 1].parse().unwrap();
     match mode {
-        PositionMode => {
+        PositionMode0 => {
             proggy[i as usize].parse().unwrap()
         },
-        ImmediateMode => {
+        ImmediateMode1 => {
             i
         },
     }
@@ -118,17 +114,17 @@ fn get_first_param(proggy: &[String], instruction_pos: usize, mode: ParameterMod
 fn get_second_param(proggy: &[String], instruction_pos: usize, mode: ParameterMode) -> isize {
     let i = proggy[instruction_pos + 2].parse().unwrap();
     match mode {
-        PositionMode => {
+        PositionMode0 => {
             proggy[i as usize].parse().unwrap()
         },
-        ImmediateMode => {
+        ImmediateMode1 => {
             i
         },
     }
 }
 
 fn get_third_param(proggy: &[String], instruction_pos: usize, mode: ParameterMode) -> isize {
-    if let ParameterMode::ImmediateMode = mode {
+    if let ParameterMode::ImmediateMode1 = mode {
         panic!("invalid program, third param can't be immediate mode")
     }
     proggy[instruction_pos + 3].parse().unwrap()
--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ pub mod day1;
 pub mod day2;
 pub mod day3;
 pub mod day4;
-pub mod day5;
+pub mod day5_part1;
+pub mod day5_part2;
 
 aoc_lib!{ year = 2019 }
