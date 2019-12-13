1ebe53b0ab86dcd9955b8910dd98eb0c396edd11
Sumeet Agarwal <sumeet.a@gmail.com>
2019-12-12 19:19:11 -0500

    finish day12

--- src/day12.rs
+++ src/day12.rs
@@ -1,7 +1,7 @@
 use itertools::Itertools;
 use std::collections::HashSet;
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
+use std::hash::{Hash};
+use num::integer::Integer;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct Moon {
@@ -64,45 +64,55 @@ fn solve_part2(input: &str) -> usize {
         Moon::new([pos.0, pos.1, pos.2])
     }).collect_vec();
 
-    let mut previous_states = HashSet::new();
-    previous_states.insert(hash(&moons));
-    println!("{:?}", moons);
+    let mut counts = [0, 0, 0];
+    for axis in [0, 1, 2].iter() {
+        let mut previous_states = HashSet::new();
+        previous_states.insert(get_axes(&moons, *axis));
 
-    let mut count = 0;
-    loop {
-        // apply gravity on all moon combinations
-        for (moon_1_index, moon_2_index) in (0..moons.len()).tuple_combinations() {
-            let moon_1 = moons[moon_1_index].clone();
-            let moon_2 = moons[moon_2_index].clone();
+        let mut count = 0;
+        loop {
+            // apply gravity on all moon combinations
+            for (moon_1_index, moon_2_index) in (0..moons.len()).tuple_combinations() {
+                let moon_1 = moons[moon_1_index].clone();
+                let moon_2 = moons[moon_2_index].clone();
 
-            for (pos_i, (pos_1, pos_2)) in moon_1.pos.iter().zip(moon_2.pos.iter()).enumerate() {
-                // lesser one increases...
-                if pos_1 < pos_2 {
-                    moons[moon_1_index].velocity[pos_i] += 1;
-                    moons[moon_2_index].velocity[pos_i] -= 1;
-                } else if pos_1 > pos_2 {
-                    moons[moon_1_index].velocity[pos_i] -= 1;
-                    moons[moon_2_index].velocity[pos_i] += 1;
+                for (pos_i, (pos_1, pos_2)) in moon_1.pos.iter().zip(moon_2.pos.iter()).enumerate() {
+                    // lesser one increases...
+                    if pos_1 < pos_2 {
+                        moons[moon_1_index].velocity[pos_i] += 1;
+                        moons[moon_2_index].velocity[pos_i] -= 1;
+                    } else if pos_1 > pos_2 {
+                        moons[moon_1_index].velocity[pos_i] -= 1;
+                        moons[moon_2_index].velocity[pos_i] += 1;
+                    }
                 }
             }
-        }
 
-        // apply velocity on each moon
-        for moon in moons.iter_mut() {
-            for (position, velocity) in moon.pos.iter_mut().zip(moon.velocity.iter()) {
-                *position += *velocity
+            // apply velocity on each moon
+            for moon in moons.iter_mut() {
+                for (position, velocity) in moon.pos.iter_mut().zip(moon.velocity.iter()) {
+                    *position += *velocity
+                }
+            }
+            count += 1;
+            let axes = get_axes(&moons, *axis);
+            if previous_states.contains(&axes) {
+                break
+            } else {
+                previous_states.insert(axes);
             }
         }
-        count += 1;
-        let hash = hash(&moons);
-        if previous_states.contains(&hash) {
-            break
-        } else {
-            println!("{:?}", moons);
-            previous_states.insert(hash);
-        }
+        counts[*axis] = count;
     }
-    count
+    counts[0].lcm(&counts[1]).lcm(&counts[2])
+}
+
+fn get_axes(moons: &[Moon], axis: usize) -> Vec<(isize, isize)> {
+    moons.iter().map(|moon| get_axis(moon, axis)).collect()
+}
+
+fn get_axis(moon: &Moon, axis: usize) -> (isize, isize) {
+    (moon.pos[axis], moon.velocity[axis])
 }
 
 fn potential_energy(moon: &Moon) -> usize {
@@ -111,12 +121,6 @@ fn potential_energy(moon: &Moon) -> usize {
 
 fn kinetic_energy(moon: &Moon) -> usize {
     moon.velocity.iter().map(|velocity| velocity.abs() as usize).sum()
-}
-
-fn hash<T: Hash>(t: &T) -> u64 {
-    let mut s = DefaultHasher::new();
-    t.hash(&mut s);
-    s.finish()
 }
 
 #[test]
