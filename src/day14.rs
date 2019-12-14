452d833f96b6c8f82700b422df5df5f11009cbfe
Sumeet Agarwal <sumeet.a@gmail.com>
2019-12-14 15:16:35 -0500

    Revert "added concept of waste, still doesn't work :/"
    This reverts commit 9766035a66f21c2decd2d3329c235aa4a5858d84.

--- src/day14.rs
+++ src/day14.rs
@@ -1,7 +1,5 @@
 use std::collections::HashMap;
 use itertools::Itertools;
-use defaultmap::DefaultHashMap;
-use std::cmp::min;
 
 #[aoc(day14, part1)]
 fn solve_part1(input: &str) -> usize {
@@ -17,37 +15,24 @@ fn solve_part1(input: &str) -> usize {
         let output = ReactionPart::new(parts_str.parse().unwrap(), chemical_str.into());
         (output.chemical.clone(), Reaction::new(inputs.collect(), output))
     }).collect();
-    let mut waste = Waste::new();
-    let n = num_ore_required(&ReactionPart::new(1, "FUEL".into()),
-        &reaction_by_output,
-        &mut waste);
-    println!("waste remaining @ end: {:?}", waste);
-    n
+    num_ore_required(&ReactionPart::new(1, "FUEL".into()), &reaction_by_output)
 }
 
-fn num_ore_required(target: &ReactionPart, reactions_list: &ReactionsList, waste: &mut Waste) -> usize {
-    println!("wanting to get {:?}", target);
+fn num_ore_required(target: &ReactionPart, reactions_list: &ReactionsList) -> usize {
     if target.chemical == "ORE" {
-        println!("it's just ore, so give em {} of it", target.parts);
         return target.parts
     }
 
     let reaction = reactions_list.get(&target.chemical).unwrap();
-    let num_pilfered_from_waste = waste.pilfer(&target.chemical, target.parts);
-    let num_parts_needed = target.parts - num_pilfered_from_waste;
-
-    if num_pilfered_from_waste != 0 {
-        println!("pilfered {} parts of {} to satisfy part of that", num_pilfered_from_waste, target.chemical);
-    }
-
-    let num_reactions = (num_parts_needed as f64 / reaction.output.parts as f64).ceil() as usize;
-    println!("needed {} reactions to satisfy that, after pilfering", num_reactions);
-    let wasted_parts = (num_reactions * reaction.output.parts) - num_parts_needed;
-    println!("{} new parts of {} were wasted", wasted_parts, target.chemical);
-    waste.add(target.chemical.to_string(), wasted_parts);
+    let num_reactions = if target.parts <= reaction.output.parts {
+        1
+    } else {
+        (target.parts as f64 / reaction.output.parts as f64).ceil() as usize
+    };
     let deonjida = reaction.inputs.iter().map(|input| {
-        num_ore_required(input, reactions_list, waste)
+        num_ore_required(input, reactions_list)
     }).sum::<usize>() * num_reactions;
+    println!("required {} ORE to produce {:?}", deonjida, target);
     deonjida
 }
 
@@ -78,59 +63,12 @@ impl Reaction {
     }
 }
 
-#[derive(Debug)]
-struct Waste {
-    num_parts_by_chemical: DefaultHashMap<String, usize>,
-}
-
-impl Waste {
-    fn new() -> Self {
-        Self { num_parts_by_chemical: DefaultHashMap::new(0) }
-    }
-
-    fn add(&mut self, chemical: String, num_parts: usize) {
-        self.num_parts_by_chemical[chemical] += num_parts
-    }
-
-    fn pilfer(&mut self, chemical: &str, num_parts_needed: usize) -> usize {
-        let num_parts_in_waste = &mut self.num_parts_by_chemical[chemical.into()];
-        let num_parts_to_take_from_waste = min(num_parts_needed, *num_parts_in_waste);
-        *num_parts_in_waste -= num_parts_to_take_from_waste;
-        let pilfered = num_parts_to_take_from_waste;
-        pilfered
-    }
-}
-
 #[test]
-fn p1_a() {
+fn p1() {
     assert_eq!(solve_part1("10 ORE => 10 A
 1 ORE => 1 B
 7 A, 1 B => 1 C
 7 A, 1 C => 1 D
 7 A, 1 D => 1 E
-7 A, 1 E => 1 FUEL"), 31);
-}
-
-#[test]
-fn p1_b() {
-    assert_eq!(solve_part1("9 ORE => 2 A
-8 ORE => 3 B
-7 ORE => 5 C
-3 A, 4 B => 1 AB
-5 B, 7 C => 1 BC
-4 C, 1 A => 1 CA
-2 AB, 3 BC, 4 CA => 1 FUEL"), 165)
-}
-
-#[test]
-fn p1_c() {
-    assert_eq!(solve_part1("157 ORE => 5 NZVS
-165 ORE => 6 DCFZ
-44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
-12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
-179 ORE => 7 PSHF
-177 ORE => 5 HKGWZ
-7 DCFZ, 7 PSHF => 2 XJWVT
-165 ORE => 2 GPVTF
-3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"), 13312)
+7 A, 1 E => 1 FUEL"), 31)
 }
