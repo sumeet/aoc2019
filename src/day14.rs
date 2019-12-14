645f6330ed726686d5ba5e34d5b4a689e2007690
Sumeet Agarwal <sumeet.a@gmail.com>
2019-12-14 17:27:42 -0500

    hahahaha ok!

--- src/day14.rs
+++ src/day14.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use itertools::Itertools;
-use num::Integer;
+use defaultmap::DefaultHashMap;
+use std::cmp::min;
 
 #[aoc(day14, part1)]
 fn solve_part1(input: &str) -> usize {
@@ -16,30 +17,28 @@ fn solve_part1(input: &str) -> usize {
         let output = ReactionPart::new(parts_str.parse().unwrap(), chemical_str.into());
         (output.chemical.clone(), Reaction::new(inputs.collect(), output))
     }).collect();
-    num_ore_required(&ReactionPart::new(1, "FUEL".into()), &reaction_by_output)
+    let mut waste = Waste::new();
+    num_ore_required(&ReactionPart::new(1, "FUEL".into()),
+        &reaction_by_output,
+        &mut waste)
 }
 
-fn num_ore_required(target: &ReactionPart, reactions_list: &ReactionsList) -> usize {
+fn num_ore_required(target: &ReactionPart, reactions_list: &ReactionsList, waste: &mut Waste) -> usize {
     if target.chemical == "ORE" {
         return target.parts
     }
 
     let reaction = reactions_list.get(&target.chemical).unwrap();
-    let (num_reactions, num_unused) = div_ceil_rem(target.parts, reaction.output.parts);
-    println!("producing {} of {}", target.parts, target.chemical);
-    let deonjida = reaction.inputs.iter().map(|input| {
-        num_ore_required(input, reactions_list)
-    }).sum::<usize>() * num_reactions;
-    println!("required {} ORE to produce {:?}", deonjida, target);
-    deonjida
-}
+    let num_pilfered_from_waste = waste.pilfer(&target.chemical, target.parts);
+    let num_parts_needed = target.parts - num_pilfered_from_waste;
 
-fn div_ceil_rem(top: usize, bot: usize) -> (usize, usize) {
-    let (mut quotient, remainder) = top.div_rem(&bot);
-    if remainder > 0 {
-        quotient += 1
-    }
-    (quotient, remainder)
+    let num_reactions = (num_parts_needed as f64 / reaction.output.parts as f64).ceil() as usize;
+    let wasted_parts = (num_reactions * reaction.output.parts) - num_parts_needed;
+    waste.add(target.chemical.to_string(), wasted_parts);
+    reaction.inputs.iter().map(|input| {
+        let input = ReactionPart::new(input.parts * num_reactions, input.chemical.clone());
+        num_ore_required(&input, reactions_list, waste)
+    }).sum()
 }
 
 // keyed by output
@@ -69,6 +68,28 @@ impl Reaction {
     }
 }
 
+#[derive(Debug)]
+struct Waste {
+    num_parts_by_chemical: DefaultHashMap<String, usize>,
+}
+
+impl Waste {
+    fn new() -> Self {
+        Self { num_parts_by_chemical: DefaultHashMap::new(0) }
+    }
+
+    fn add(&mut self, chemical: String, num_parts: usize) {
+        self.num_parts_by_chemical[chemical] += num_parts
+    }
+
+    fn pilfer(&mut self, chemical: &str, num_parts_needed: usize) -> usize {
+        let num_parts_in_waste = &mut self.num_parts_by_chemical[chemical.into()];
+        let num_parts_to_take_from_waste = min(num_parts_needed, *num_parts_in_waste);
+        *num_parts_in_waste -= num_parts_to_take_from_waste;
+        num_parts_to_take_from_waste
+    }
+}
+
 #[test]
 fn p1_a() {
     assert_eq!(solve_part1("10 ORE => 10 A
@@ -76,7 +97,7 @@ fn p1_a() {
 7 A, 1 B => 1 C
 7 A, 1 C => 1 D
 7 A, 1 D => 1 E
-7 A, 1 E => 1 FUEL"), 31)
+7 A, 1 E => 1 FUEL"), 31);
 }
 
 #[test]
@@ -91,9 +112,14 @@ fn p1_b() {
 }
 
 #[test]
-fn test_div_ceil_rem() {
-    // suppose 9 ORE => 2 A
-    // and we want just 3 A
-    let (num_reactions_needed, num_extra) = div_ceil_rem(3, 2);
-    assert_eq!((num_reactions_needed, num_extra), (2, 1));
+fn p1_c() {
+    assert_eq!(solve_part1("157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF
+177 ORE => 5 HKGWZ
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"), 13312)
 }
