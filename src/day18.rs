49fd5d8b4e8f8290e0746293027435e506da57b0
Sumeet Agarwal <sumeet.a@gmail.com>
2019-12-22 21:09:10 -0500

    whoops forgot input

--- src/day18.rs
+++ src/day18.rs
@@ -192,6 +192,8 @@ impl SpaceType {
 #[aoc(day18, part1)]
 fn solve_part1(input: &str) -> usize {
     let mut map = Map::parse(input);
+    
+    // need this threadpool stuff to increase stack size
     let pool = ThreadPoolBuilder::new()
         .stack_size(1024 * 1024 * 1000)
         .build()
@@ -200,6 +202,10 @@ fn solve_part1(input: &str) -> usize {
         let paths = map.find_min_path();
         paths.map(|p| p.num_moves).unwrap()
     })
+    
+//    let paths = map.find_min_path();
+//    paths.unwrap().num_moves
+
     //paths.iter().map(|path| path.num_moves).min().unwrap()
     //    let destinations = map.destinations_from_current_pos(HashSet::new());
     //    format!("{:?}", destinations)
